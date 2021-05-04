b0b3d47e56fd90cb06afdacb19cdafe738238462
André de Albuquerque <itstheandre@gmail.com>
2021-05-04 20:32:17 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// const obj = {
+//   year: 2020,
+//   hello: "banana",
+//   jakarta: "city ",
+// };
+
+// const { year, ...words } = obj;
+// console.log(words);
+// console.log(words);
+// console.log(obj);
+
+// const arr = ["the office", "bears", "beets", "battlestar galactica"];
+// function returnsFancyStuff() {
+//   return ["the office", "bears", "beets", "battlestar galactica"];
+// }
+
+// // const [one, two] = arr;
+// const [
+//   show,
+//   animal,
+//   vegetableQuestionMark,
+//   accordingToFilipeSomeFutureThing,
+// ] = returnsFancyStuff();
+
+// // const show = arr[0]
+// // const animal = arr[1]
+// // const vegetableQuestionMark = arr[2]
+
+// function returnsTwoElementArray(arg) {
+//   return [arg, console.log];
+// }
+
+// const [value, print] = returnsTwoElementArray(3);
+
+// print(value);
+
+// // const log = console.log;
+
+function Footer(props) {
+  return <footer>Hello from footer: {props.counter}</footer>;
+}
 
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  // let counter = 0;
+  function increment() {
+    // counter++;
+    setCounter(counter + 1);
+  }
+
+  function decrement() {
+    setCounter(counter - 1);
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <Footer counter={counter} />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{counter}</p>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React {counter}
         </a>
       </header>
     </div>
@@ -23,3 +78,23 @@ function App() {
 }
 
 export default App;
+
+// const span = document.querySelector("span");
+// const plusButton = document.querySelector(".plus");
+// const minusButton = document.querySelector(".minus");
+// const footer = document.querySelector("footer");
+
+// plusButton.addEventListener("click", () => {
+//   const spanValue = parseInt(span.innerText);
+
+//   const footerVal = parseInt(footer.innerText);
+
+//   span.innerText = spanValue ? spanValue + 1 : 1;
+//   footer.innerText = footerVal ? footerVal + 1 : 1;
+// });
+
+// minusButton.addEventListener("click", () => {
+//   const spanValue = parseInt(span.innerText);
+
+//   span.innerText = spanValue ? spanValue - 1 : 0;
+// });
